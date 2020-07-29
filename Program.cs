aa9c23872f7dbca37b99008c81fa128b080167c6
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2020-07-29 17:42:16 +0400

    Бета-версия 2
    Добавлен тригонометрический калькулятор, разработаны переключения между режимами и исключения в случае ввода недоступного в калькуляторе режима. А также были исправлены некоторые баги;-)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 namespace csharp_dotnetcore_calculator
 {
     class Program
@@ -9,6 +10,7 @@ namespace csharp_dotnetcore_calculator
         }
         public static float subtraction(float first,float second)
         {
+
             return first - second;//Вычитание
         }
         public static float multiplication(float first,float second)
@@ -17,12 +19,16 @@ namespace csharp_dotnetcore_calculator
         }
         public static float division(float first,float second)
         {
-            return first / second;
+            if(second != 0 && first !=0)
+            {
+                return first / second;
+            }
+            else{
+                return 0;
+            }
         }
-
-        static void Main(string[] args)
+        public static void MathCalc()
         {
-            Console.WriteLine("Добро пожаловать в калькулятор!");
             while (true)
             {
                 Console.WriteLine("Введите первое число:");
@@ -61,6 +67,74 @@ namespace csharp_dotnetcore_calculator
 
                 
             }
+        }
+        public static void TrigonometryCalc()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите название тригонометрической функции:");
+                string o = Console.ReadLine();
+                Console.WriteLine("Введите число:");
+                double s = double.Parse(Console.ReadLine());
+
+                double result = 0d;
+
+                if (o == "cos")
+                {
+                    result = Math.Cos(s);
+                }
+                else if(o == "sin")
+                {
+                    result = Math.Sin(s);
+                }
+                else if(o == "tan")
+                {
+                    result = Math.Tan(s);
+                }
+                else if(o == "acos")
+                {
+                    result = Math.Acos(s);
+                }
+                else if(o == "asin")
+                {
+                    result = Math.Asin(s);
+                }
+                else if(o == "atan")
+                {
+                    result = Math.Atan(s);
+                }
+                else
+                      throw new Exception($"Unexpected operation '{o}'");
+                
+                string value = $"Результат функции: {result}";
+                Console.WriteLine(value);
+
+
+            }
+        }
+
+        public static void NotAvaliable()
+        {
+            Console.WriteLine("Запрашиваемый вами режим не доступен. Через 10 секунд программа будет закрыта! Просим вас перезапустить программу;-)");
+            Thread.Sleep(10000);
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в калькулятор!");
+
+            Console.WriteLine("Введите цифру, включая режим калькулятора(0-традиционный,1-тригонометрический)");
+
+            int mode = int.Parse(Console.ReadLine());
+
+            switch(mode)
+            {
+                case 0: MathCalc(); break;
+                case 1: TrigonometryCalc(); break;
+                default: NotAvaliable(); break;
+            }
+            
         }
     }
 }
